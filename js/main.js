dc4c17b02b5a2be0cda24e120d7264061209f1d2
Jesus Medina <chuymedina96@gmail.com>
2017-06-29 01:06:45 +0000

    more

--- js/main.js
+++ js/main.js
@@ -5,43 +5,57 @@
         a("html, body").stop().animate({
             scrollTop: a(c.attr("href")).offset().top - 50
         }, 1250, "easeInOutExpo"), b.preventDefault()
-    }), a("body").scrollspy({
+    }); 
+    a("body").scrollspy({
         target: ".navbar-fixed-top",
         offset: 51
-    }), a(".navbar-collapse ul li a").click(function() {
+    });
+a(".navbar-collapse ul li a").click(function() {
         a(".navbar-toggle:visible").click()
-    }), a("#mainNav").affix({
+    });
+a("#mainNav").affix({
         offset: {
             top: 100
         }
-    })
+    });
 }(jQuery), 
 
 $(document).ready(function() {
     $(".plusminus").parent().parent().click(function() {
         $(this).hasClass("collapsed") ? $(this).find("i").removeClass("fa-chevron-circle-right").addClass("fa-chevron-circle-down") : $(this).find("i").removeClass("fa-chevron-circle-down").addClass("fa-chevron-circle-right");
     })
-}), $("#btn").click(function(a) {
-    $("#fancy, #btn").fadeOut(1000, function() {
-        $("#bank, #btn-bk").fadeIn(1000);
-    })
-}), $("#btn-bk").click(function(a) {
+});
+$("#btn").click(function(a) {
+        $("#fancy, #btn").fadeOut(1000, function() {
+            $("#bank, #btn-bk").fadeIn(1000);
+        });
+});
+
+$("#btn-bk").click(function(a) {
     $("#bank, #btn-bk").fadeOut(1000, function() {
         $("#fancy, #btn").fadeIn(1000);
     })
-}), $("#understand").click(function() {
+});
+
+$("#understand").click(function() {
     $("#btn-bk, #bank").fadeOut(1000, function() {
         $("#btn-back").fadeIn(1000), $("#container").fadeIn(1000)
     })
-}), $("#btn-back").click(function() {
+});
+
+$("#btn-back").click(function() {
     $("#container, #btn-back").fadeOut(1000, function() {
         $("#bank, #btn-bk").fadeIn(1000);
     })
-}), $("#sixteen").click(function() {
+});
+
+$("#sixteen").click(function() {
     $("#container, #btn-back").fadeOut(1000, function() {
         $("#results, #btn-backItUp").fadeIn(1000);
     })
-}), $("#btn-backItUp").click(function() {
+});
+
+$("#btn-backItUp").click(function() {
     $("#results, #btn-backItUp").fadeOut(1000, function() {
         $("#container, #btn-back").fadeIn(1000);
     })
